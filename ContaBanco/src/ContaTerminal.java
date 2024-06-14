f7b5bfca3d28c2e678fb8576e0b5a4aa94f2b9b8
Carlos <kaka_ttr@outlook.com.br>
2024-06-14 16:12:02 -0300

    commit conta Terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String [] args) throws Exception {
+       Double saldo = 237.48;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    
+    System.out.println("Por favor, digite o seu nome");
+    String cliente = scanner.next();
+    System.out.println("agência");
+    String agência = scanner.next();
+    System.out.println("conta");
+    String conta = scanner.next();
+    System.out.println("saldo");
+    
+    System.out.println("Olá " +  cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agência + " conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+    
+
+
     }
+
+
 }
 
